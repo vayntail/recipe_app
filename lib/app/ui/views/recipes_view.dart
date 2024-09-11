53b74426222612b9c2b7778346a44aeec56e1e69
vayntail <113714157+vayntail@users.noreply.github.com>
2024-09-11 08:07:08 -0400

    newer fix

--- lib/app/ui/views/recipes_view.dart
+++ lib/app/ui/views/recipes_view.dart
@@ -1,86 +1,155 @@
 import 'package:flutter/material.dart';
+import 'package:recipe_app/app/model/recipe.dart';
 import 'package:recipe_app/app/ui/components/recipe_button1.dart';
-import 'package:recipe_app/app/ui/components/search_tags_container.dart';
 import 'package:recipe_app/app/ui/screens/new_recipe_screen.dart';
-import 'package:recipe_app/app/recipedb_operations.dart';
 
 class RecipesView extends StatefulWidget {
   const RecipesView({super.key});
 
   @override
-  _RecipesViewState createState() => _RecipesViewState();
+  State<RecipesView> createState() => _RecipesViewState();
 }
 
 class _RecipesViewState extends State<RecipesView> {
-  final RecipeOperations _recipeOperations =
-      RecipeOperations(); // Initialize DB operations
-  late Future<List<Map<String, dynamic>>> _recipesFuture;
+  /// Call recipesList from recipe.dart
 
-  @override
-  void initState() {
-    super.initState();
-    _recipesFuture =
-        _recipeOperations.getRecipes(); // Fetch recipes when state initializes
+  /// List of Tags created from the Recipes List
+  final List<String> tagsList = [];
+  void setTagsList() {
+    for (var recipe in recipesList) {
+      for (var tag in recipe.tags) {
+        if (!tagsList.contains(tag)) {
+          tagsList.add(tag);
+        }
+      }
+    }
+  }
+
+  String selectedTag = ""; // Selected Tag
+  bool searching = false;
+  /// Search bar stuff
+  /// 
+  List<Recipe> searchResults = [];
+  void onQueryChanged(String query) {
+    setState(() {
+
+      if (query==""){
+        searching = false;
+      }
+      else {
+        searching = true;
+      }
+      searchResults = recipesList
+          .where((recipe) =>
+              recipe.name.toLowerCase().contains(query.toLowerCase()))
+          .toList();
+    });
   }
 
   @override
   Widget build(BuildContext context) {
+    setTagsList(); // Set the List
+
+    /// filtered list of Recipes
+    List<Recipe> filteredRecipes = [];
+    void setFilteredRecipes() {
+      // debugPrint("searching: ${searching}, SelectedTag: ${selectedTag}, searchBarFocused: ${searchBarFocused}, searchResults: ${searchResults}");
+
+      // Shows filtered results from both search bar and selected Tag.
+      if (selectedTag.isEmpty) {
+        if (searchResults.isNotEmpty) {
+          filteredRecipes = searchResults;
+        } else {
+          if (searching){
+            filteredRecipes = searchResults;
+          }
+          else {
+            filteredRecipes = recipesList;
+          }
+        }
+      } else { // selected tag is Not empty
+        for (var recipe in recipesList) {
+          for (var tag in recipe.tags) {
+            if (selectedTag == tag) {
+              if (!searching){
+                filteredRecipes.add(recipe);
+              }
+              else {
+                if (searchResults.contains(recipe)){
+                  filteredRecipes.add(recipe);
+                }
+              }
+            } else {
+              filteredRecipes.remove(recipe);
+            }
+          }
+        }
+      } 
+    }
+    setFilteredRecipes();
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Recipes'),
         actions: <Widget>[
-          // Add new button
+          /// Add new button
           IconButton(
-            icon: const Icon(Icons.add),
-            tooltip: 'New Recipe',
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) =>
-                        const NewRecipeScreen()), // Go to new recipe screen
-              );
-            },
-          ),
+              icon: const Icon(Icons.add),
+              tooltip: 'New Recipe',
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            const NewRecipeScreen()) // Go to new recipe screen
+                    );
+              })
         ],
       ),
       body: Column(
-        children: <Widget>[
-          const SearchTagsContainer(),
-          Expanded(
-            child: FutureBuilder<List<Map<String, dynamic>>>(
-              future: _recipesFuture,
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const Center(child: CircularProgressIndicator());
-                } else if (snapshot.hasError) {
-                  return Center(child: Text('Error: ${snapshot.error}'));
-                } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                  return const Center(child: Text('No Recipes Found'));
-                } else {
-                  final recipes = snapshot.data!;
-                  return ListView.builder(
-                    padding: const EdgeInsets.all(8),
-                    itemCount: recipes.length,
-                    itemBuilder: (context, index) {
-                      final recipe = recipes[index];
-                      return RecipeButton1(
-                        name: recipe['recipe_name'] ??
-                            'No Name', // Ensure default value
-                        description:
-                            recipe['recipe_description'] ?? 'No Description',
-                        minutes: (recipe['hours'] ?? 0) * 60 +
-                            (recipe['minutes'] ?? 0), // Handle null
-                        imagePath: recipe['image_path'] ?? '', // Handle null
-                      );
-                    },
-                  );
-                }
-              },
-            ),
+
+        crossAxisAlignment: CrossAxisAlignment.start, children: [
+        /// Search bar
+        Container(
+        padding: EdgeInsets.only(left: 8, right: 8),
+          width: 300,
+          child: TextField(
+            onChanged: onQueryChanged,
+            decoration: const InputDecoration(
+                labelText: 'Search',
+                border: OutlineInputBorder(),
+                prefixIcon: Icon(Icons.search),
+                floatingLabelBehavior: FloatingLabelBehavior.never),
           ),
-        ],
-      ),
+        ),
+
+        /// Tags Container
+        Row(
+          children: tagsList
+              .map((tag) => FilterChip(
+                selectedColor: const Color.fromARGB(255, 223, 212, 212),
+                  showCheckmark: false,
+                  selected: selectedTag.contains(tag),
+                  label: Text(tag),
+                  onSelected: (selected) {
+                    setState(() {
+                      if (selected) {
+                        selectedTag = tag;
+                      } else {
+                        selectedTag = "";
+                      }
+                    });
+                  }))
+              .toList(),
+        ),
+        Expanded(
+          child: ListView.builder(
+              itemCount: filteredRecipes.length,
+              itemBuilder: (context, index) {
+                return RecipeButton1(recipe: filteredRecipes[index]);
+              }),
+        ),
+      ]),
     );
   }
 }
